2e0e88c6561fccd1352f9d8d791d02f24d4a0c63
Oncu <oohancan@gmail.com>
2022-06-04 20:21:54 +0300

    Initial Commit

---
+++ FirebaseChatTutorial/FirebaseChatTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseChatTutorialApp.swift
+//  FirebaseChatTutorial
+//
+//  Created by Oncu Ohancan on 4.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseChatTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
