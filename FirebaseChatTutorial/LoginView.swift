d4964589c3b73f5c1e9b5ef986a92585ab906aff
Oncu <oohancan@gmail.com>
2022-06-09 12:01:18 +0300

    add imagePicker and func persistImageToStorage

--- FirebaseChatTutorial/LoginView.swift
+++ FirebaseChatTutorial/LoginView.swift
@@ -11,6 +11,7 @@ import Firebase
 class FirebaseManager: NSObject {
     
     let auth: Auth
+    let storage: Storage
     
     static let shared = FirebaseManager()
     
@@ -18,6 +19,7 @@ class FirebaseManager: NSObject {
         FirebaseApp.configure()
        
        self.auth = Auth.auth()
+       self.storage = Storage.storage()
        
        super.init()
     }
@@ -28,6 +30,7 @@ struct LoginView: View {
     @State var isLoginMode = false
     @State var email = ""
     @State var password = ""
+    @State var shouldShowImagePicker = false
     
     init() {
         FirebaseApp.configure()
@@ -48,13 +51,27 @@ struct LoginView: View {
                     
                     if !isLoginMode {
                         Button {
-                            
-                        } label: {Image(systemName: "person.fill")
-                            .font(.system(size: 64))
-                            .padding()
+                            shouldShowImagePicker.toggle()
+                        } label: {
+                            VStack {
+                                if let image = self.image {
+                                    Image(uiImage: image)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 128, height: 128)
+                                        .cornerRadius(64)
+                                } else {
+                                    Image(systemName: "person.fill")
+                                    .font(.system(size: 64))
+                                    .padding()
+                                    .foregroundColor(Color(.label))
+                            }
                         }
+                            .overlay(RoundedRectangle(cornerRadius: 64)
+                                .stroke(Color.black, lineWidth: 3)
+                                     )
+                    }
                 }
-                
                     Group {
                         TextField("Email", text: $email)
                                 .keyboardType(.emailAddress)
@@ -87,7 +104,14 @@ struct LoginView: View {
             .ignoresSafeArea())
         }
         .navigationViewStyle(StackNavigationViewStyle())
+        .fullScreenCover(isPresented: $shouldShowImagePicker,
+            onDismiss: nil) {
+            ImagePicker(image: $image)
+        }
     }
+    
+    @State var image: UIImage?
+    
     private func handleAction() {
         if isLoginMode {
             print("Should log into Firebase with existing credentials")
@@ -125,9 +149,34 @@ struct LoginView: View {
             print("Successfully created user: \(result?.user.uid ?? "")")
             
             self.loginStatusMessage = "Successfully created user: \(result?.user.uid ?? "")"
+            
+            self.persistImageToStorage()
         }
     }
-}
+    private func persistImageToStorage() {
+//        let filename = UUID().uuidString
+        guard let uid = FirebaseManager.shared.auth.currentUser?.uid
+        else { return }
+        
+       let ref = FirebaseManager.shared.storage.reference(withPath: uid)
+        guard let imageData = self.image?.jpegData(compressionQuality: 0.5) else { return }
+        
+        ref.putData(imageData, metadata: nil) { metadata, err in
+            if let err = err {
+                self.loginStatusMessage = "Failed to push image to Storage: \(err)"
+                return
+                }
+            ref.downloadURL { url, err in
+                if let err = err {
+                    self.loginStatusMessage = "Failed to retrieve downloadURL: \(err)"
+                    return
+                   }
+                self.loginStatusMessage = "Succesfully stored image with url: \(url?.absoluteString ?? "")"
+                print(url?.absoluteString)
+            }
+            }
+        }
+    }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView()
